2a30a575f2406f6886e32c624a1168faaee52a14
vex <106108050+VexTheDev@users.noreply.github.com>
2022-07-11 17:45:29 +0300

    first ver

---
+++ raper.py
@@ -0,0 +1,56 @@
+from tkinter import *
+from PIL import Image,ImageTk
+import pyautogui
+wn = Tk()
+
+
+def start_rape():
+    for _ in range(9):
+        pyautogui.press("r")
+        print("raped")
+
+
+
+
+icon = PhotoImage(file="C:\\Users\\lucam\\3D Objects\\screenshare raper\\img.png")
+wn.geometry("349x345")
+wn.title("Discord Raper")
+wn.iconphoto(True,icon)
+wn.config(
+    background="black"
+)
+
+img = Image.open("C:\\Users\\lucam\\3D Objects\\screenshare raper\\img.png")
+resize_image= img.resize((250,150), Image.ANTIALIAS)
+newimg = ImageTk.PhotoImage(resize_image)
+
+
+
+
+
+hi = Label(wn,
+           text="Discord ScreenShare Raper",
+           font=("Arial",15),
+           fg='#8f1810',
+           bg='black',
+           image=newimg,
+           compound='top'
+           ).pack()
+
+but = Button(wn,
+            text="Start ✔",
+            command=start_rape,
+            font=('Comic Sans',15),
+            fg='#006903',
+            bg='black'
+            ).pack(side=LEFT)
+
+#but2 = Button(wn,
+              #text="Stop ❌",
+              #command=stop_rape,
+              #font=('Comic Sans',15),
+              #fg='#8c0000',
+              #bg='black'
+              #).pack(side=RIGHT)
+
+wn.mainloop()
